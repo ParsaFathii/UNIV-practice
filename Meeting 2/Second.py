d458bc6bf90144ce1d52585a87f8de480d8685fc
parsa fathi <parsafathinasri@gmail.com>
2025-03-08 23:17:43 +0330

    none

--- Meeting 2/Second.py
+++ Meeting 2/Second.py
@@ -3,13 +3,10 @@ email = input("Please enter your Email: ")
 valid_domains = ["gmail.com", "yahoo.com", "outlook.com"]
 if "@" not in email:
     print("Error: Email must contain '@'.")
-
 elif " " in email:
     print("Error: Email must not contain spaces.")
-
 else:
     before_at, after_at = email.split("@", 1)
-
     if len(before_at) < 5:
         print("Error: The part before '@' must be at least 5 characters long.")
     elif after_at not in valid_domains:
